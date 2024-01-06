8c2bd1043d57d36fbe3de3d522a7ede9f29f0352
Bruno Martucci <brunomartucci77@gmail.com>
2024-01-06 16:44:39 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,72 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("Digite seus seis números entre 1 e 99:");
+        int[] numerosApostados = new int[6];
+        for (int i = 0; i < 6; i++) {
+            numerosApostados[i] = scanner.nextInt();
+        }
+
+        int[] numerosSorteados = gerarNumerosSorteados();
+
+        int numerosCorrespondentes = contarNumerosCorrespondentes(numerosApostados, numerosSorteados);
+
+        validarPremio(numerosCorrespondentes);
+
+        System.out.println("Números sorteados: " + Arrays.toString(numerosSorteados));
+
+        scanner.close();
+    }
+    private static int[] gerarNumerosSorteados() {
+        Random random = new Random();
+        int[] numerosSorteados = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+            numerosSorteados[i] = random.nextInt(99) + 1;
+        }
+
+        return numerosSorteados;
+    }
+
+
+    private static int contarNumerosCorrespondentes(int[] apostados, int[] sorteados) {
+        int contagem = 0;
+
+        for (int numeroApostado : apostados) {
+            for (int numeroSorteado : sorteados) {
+                if (numeroApostado == numeroSorteado) {
+                    contagem++;
+                    break;
+                }
+            }
+        }
+
+        return contagem;
+    }
+
+
+    private static void validarPremio(int acertos) {
+        switch (acertos) {
+            case 3:
+                System.out.println("Parabéns! Você ganhou um terno!");
+                break;
+            case 4:
+                System.out.println("Parabéns! Você ganhou uma quadra!");
+                break;
+            case 5:
+                System.out.println("Parabéns! Você ganhou uma quina!");
+                break;
+            case 6:
+                System.out.println("Parabéns! Você ganhou a sena!");
+                break;
+            default:
+                System.out.println("Infelizmente, você não ganhou nenhum prêmio. Tente novamente!");
         }
     }
 }
